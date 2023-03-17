d5b957bfeb7e2820aac38af0e3bde17fcd81987c
francisthore <thorefrancis@gmail.com>
2023-03-17 16:05:05 -0700

    cisfun

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,11 @@ int main(void)
 	while (i < 10)
 	{
 		putchar('0' + digits[i]);
-		putchar(',');
-		putchar(' ');
+		if (i !=9)
+		{	
+			putchar(',');
+			putchar(' ');
+		}
 		i++;
 	}
 	putchar('\n');

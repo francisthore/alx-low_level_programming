45a7de0a87d52701f9b81f15a371b2ca233993ba
francisthore <thorefrancis@gmail.com>
2023-03-17 15:46:28 -0700

    cisfun

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char str[26] = {
+	'a', 'b', 'c', 'd', 'e',
+	'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+	'u', 'v', 'w', 'x', 'y', 'z' };
+
+	i = 25;
+	while (i > 0)
+	{
+		putchar(str[i]);
+		/*putchar('\n');*/
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}

f1dfe95bf8de9aa2ddb3452d03bb44c49ef3fcbe
francisthore <thorefrancis@gmail.com>
2023-03-17 14:02:11 -0700

    cisfun

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 	/*Declaring my variable and array and initializing*/
 	int i = 0;
 	char str[52] = {
-	'a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
 	'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 	'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
 	'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',

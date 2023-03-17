39e6d5aeed298d5a3f0b17590267c14a61f37613
francisthore <thorefrancis@gmail.com>
2023-03-17 16:01:54 -0700

    cisfun

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 	int i = 0;
-	char digits[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int digits[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 	while (i < 10)
 	{
-		putchar(digits[i]);
+		putchar('0' + digits[i]);
 		putchar(',');
 		putchar(' ');
 		i++;

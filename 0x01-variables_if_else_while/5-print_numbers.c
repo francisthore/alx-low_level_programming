123d8370aea38db72899d76d4027a496de514a95
francisthore <thorefrancis@gmail.com>
2023-03-17 15:12:41 -0700

    cisfun

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int digits[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	while (i < 10)
+	{
+		putchar(digits[i]);
+	}
+	putchar('\n');
+	return (0);
+}

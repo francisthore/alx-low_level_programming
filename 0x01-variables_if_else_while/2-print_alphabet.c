ed77d679c958dca30ac00333d4fe2d5fb8333f17
francisthore <thorefrancis@gmail.com>
2023-03-17 13:28:10 -0700

    cisfun

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,7 @@ int main(void)
 	int i;
 	char str[26] = {'a', 'b', 'c', 'd', 'e',	
 	'f', 'g', 'h', 'i', 'j', 'k', 'l',	
-	'm', 'n', 'o', 'p', 'r', 's', 't',	
+	'm', 'n', 'o', 'p', 'q', 'r', 's', 't',	
 	'u', 'v', 'w', 'x', 'y', 'z'};
 	
 	i = 0;

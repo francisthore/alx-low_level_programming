25ddc83f05fc95025fe86d0059062e242940e705
francisthore <thorefrancis@gmail.com>
2023-03-17 14:24:19 -0700

    cisfun

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char str[26] = {
+	'a', 'b', 'c', 'd', 'e',
+	'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+	'u', 'v', 'w', 'x', 'y', 'z' };
+
+	i = 0;
+	while (i < 26)
+	{
+		if (str[i] != 'e')
+		{
+			putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

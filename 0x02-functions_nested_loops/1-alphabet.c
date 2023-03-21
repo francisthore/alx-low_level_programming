3bbf212beef5b6ad741ebd8646b52c954a7b29a2
francisthore <thorefrancis@gmail.com>
2023-03-21 15:18:56 -0700

    Nested loops & functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
-/* A fuction that prints the alphabet when called */
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Description: This function prints out the alphabet in low caps
+ * Return: void
+ */
 void print_alphabet(void)
 {
-        int i;
-        char letters[26] = {
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                'l', 'm' , 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                'w', 'x', 'y', 'z' };
-        i = 0;
-
-        while (i < 26)
-        {
-                putchar(letters[i]);
-                i++;
-        }
+	int i;
+	char letters[26] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z' }
+	i = 0;
+	while (i < 26)
+	{
+		putchar(letters[i]);
+		i++;
+	}
 	putchar('\n');
 }

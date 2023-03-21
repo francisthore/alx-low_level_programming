d75886e712b28f34a9dde9be405d9802027a7a25
francisthore <thorefrancis@gmail.com>
2023-03-21 16:32:36 -0700

    Nested loops & functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * print_sign - prints sign of num
  * @n: an int that is checked sign
@@ -10,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar('-');
+		putchar('-');
 		return (-1);
 	}
 }

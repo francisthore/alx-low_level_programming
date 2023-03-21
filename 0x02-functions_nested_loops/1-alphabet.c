381f1775a5bda9f6f54cc1f67ff25d8d1eacae1c
francisthore <thorefrancis@gmail.com>
2023-03-21 14:25:18 -0700

    Nested loops & functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,19 @@
 
 void print_alphabet(void)
 {
-	/*some code */
+        int i;
+        char alphabet[26] = {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                'l', 'm' , 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                'w', 'x', 'y', 'z' };
+        i = 0;
+
+        while (i < 26)
+        {
+                putchar(alphabet[i]);
+                i++;
+        }
+        putchar('\n');
 }
 
 int main(void)

e91560ca60b1348ac527a1ad992e930c38243dae
francisthore <thorefrancis@gmail.com>
2023-03-21 14:03:24 -0700

    Nested loops & functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - entry pt
+ *
+ * Return:  always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * My own header file
+ *
+ * My own functions will be declared in her
+ */
+
+/* A fuction that prints the alphabet when called */
+void print_alphabet(void)
+{
+	int i;
+	char alphabet[26] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm' , 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z' };
+	i = 0;
+	/* Iterating through the alphabet */
+	while (i < 26)
+	{
+		putchar(alphabet[i]);
+		i++;
+	}
+	putchar('\n');
+}

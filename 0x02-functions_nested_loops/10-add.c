f8b1c04a97d749099740ba402b1618ac0729e2f2
francisthore <thorefrancis@gmail.com>
2023-03-21 17:29:51 -0700

    Nested loops & functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - add 2 numns
+ * @num1: number 1
+ * @num2: number 2
+ *
+ * Description: Take 2 numnsand add
+ * Return: int
+ */
+int add(int num1, int num2)
+{
+	int sum;
+	sum = num1 + num2;
+	return sum;
+}

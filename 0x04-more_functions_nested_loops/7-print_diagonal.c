96c56480237fdb5a44d2b2ce234e8224cac3fff7
francisthore <thorefrancis@gmail.com>
2023-03-23 15:59:57 -0700

    Debugging

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_diagonal - prints \
+ * @n: integer determining how many times a \ is printed
+ * Descriptiom: prints \ n x
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			j = 0;
+
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+}

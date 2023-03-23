d195ab850be31ef84ec2f00879132ee0c2bedc25
francisthore <thorefrancis@gmail.com>
2023-03-23 14:01:40 -0700

    Debugging

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints a number patter 0-14 10x
+ * Description: prints 0-14 10x
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar('0' + j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

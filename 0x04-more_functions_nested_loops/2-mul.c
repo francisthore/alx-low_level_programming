8bf4c99398887595ecf46a55e11232ef93bbb9d1
francisthore <thorefrancis@gmail.com>
2023-03-23 13:13:21 -0700

    Debugging

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - multiplies 2 numbers
+ * @a: first number
+ * @b: second number
+ * Description: takes in two numbers, multiplies them and return the answer
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int ans;
+
+	ans = a * b;
+
+	return (ans);
+}

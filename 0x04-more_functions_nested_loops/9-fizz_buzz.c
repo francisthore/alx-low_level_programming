1bf4d2bdca821a5023d06f034194b708eaac86d8
francisthore <thorefrancis@gmail.com>
2023-03-24 15:14:44 -0700

    Loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,11 @@ void FizzBuzz(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i == 100)
+		{
+			printf("%d", i);
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 			putchar(' ');

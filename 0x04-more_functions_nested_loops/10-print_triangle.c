7df73fa30b6bb0740cc6576a1db7985c8f0a82a6
francisthore <thorefrancis@gmail.com>
2023-03-24 14:48:17 -0700

    Loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,26 +9,32 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	i = 1;
-
-	while (i <= size)
+	if (size <= 0)
 	{
-		j = size - i;
-
-		while (j > 0)
-		{
-			_putchar(' ');
-			j--;
-		}
-		k = 1;
-
-		while (k <= i)
-		{
-			_putchar('#');
-			k++;
-		}
 		_putchar('\n');
-		i++;
 	}
+	else
+	{
+		i = 1;
 
+		while (i <= size)
+		{
+			j = size - i;
+
+			while (j > 0)
+			{
+				_putchar(' ');
+				j--;
+			}
+			k = 1;
+
+			while (k <= i)
+			{
+				_putchar('#');
+				k++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
 }

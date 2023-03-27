061d2f38f464f10d791a0c647a5404e9faffcb7d
francisthore <thorefrancis@gmail.com>
2023-03-27 16:01:36 -0700

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,16 +9,13 @@
 
 void puts2(char *str)
 {
-	int i, j, MAX;
+	int i, MAX;
 
-	MAX = strlen(str);
-	j = 0;
+	MAX = strlen(str);	
 
-	for (i = 0; i <= MAX; i++)
+	for (i = 0; i <= MAX; i += 2)
 	{
-		_putchar(str[j]);
-		if (str[i] != '\0')
-			j += 2;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }

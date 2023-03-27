203ac0b48851eaadf287d80090b2bbdefebd0917
francisthore <thorefrancis@gmail.com>
2023-03-27 12:42:02 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of 2 ints
+ * @a: pointer to a
+ * @b: pointer to b
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int *tmp, tmp2; /* declare a temporary pointer */
+
+	tmp = a;
+	tmp2 = *b;
+
+	*tmp = *a; /* now the tmp pointer has the address of a */
+	
+	*b = *tmp; /* now b = a */
+	*a = tmp2;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 void _putchar(int ch);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);

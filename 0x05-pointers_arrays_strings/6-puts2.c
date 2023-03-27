cfcac1817010c146ce076ab026848365401bf113
francisthore <thorefrancis@gmail.com>
2023-03-27 15:45:01 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints evry other character
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i, j, MAX;
+
+	MAX = strlen(str);
+	j = 0;
+
+	for (i = 0; i <= MAX; i++)
+	{
+		_putchar(str[j]);
+		if (j != MAX)
+			j += 2;
+	}
+	_putchar('\n');
+}

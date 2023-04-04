e0e9148d4f14ffba3a8a71e47f8f6e7b4f350b57
francisthore <thorefrancis@gmail.com>
2023-04-04 04:28:47 -0700

    pointers to pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include <strig.h>
+
+/**
+ * _strstr - locates substring
+ * @haystack: main string
+ * @needle: substring
+ * Return: pointer to substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *prt = strstr(haystack, needle);
+
+	return (ptr);
+}

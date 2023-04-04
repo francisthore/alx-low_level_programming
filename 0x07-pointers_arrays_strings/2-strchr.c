602885bb5b8ba58f86c06bce4d8a9bb901e4997f
francisthore <thorefrancis@gmail.com>
2023-04-04 03:58:57 -0700

    pointers to pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strchr - returns pointer to first occurance of character (search)
+ * @s: string to look character from
+ * @c: character
+ * Return: pointer or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *ptr;
+	ptr = strchr(s, c);
+
+	return (ptr);
+}

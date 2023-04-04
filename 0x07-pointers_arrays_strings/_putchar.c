6ef89eff0be05673e07279ecac8a35cdc65def91
francisthore <thorefrancis@gmail.com>
2023-04-04 04:52:10 -0700

    pointers to pointers

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _putchar - my own function calling the c putchar
+ * @ch: a character
+ *
+ * Description: take in a character
+ *
+ * Return: int
+ */
+
+int _putchar(int ch)
+{
+	putchar(ch);
+	return (0);
+}

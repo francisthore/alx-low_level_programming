b79304cf22577edc680be1b353cd6e4f7501d132
francisthore <thorefrancis@gmail.com>
2023-04-06 15:09:42 -0700

    l

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,34 @@
+#include <string.h>
+
+int cmp(char *s1, char *s2, int i, int j);
+
+/**
+ * wildcmp - compares two strings to check if they are identical
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if idential, else 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return (cmp(s1, s2, 0, 0));
+}
+
+/**
+ * cmp - compares two strings
+ * @s1: first string
+ * @s2, second string
+ * @i: recursion looper (s1 index)
+ * @j: recursion looper (s2 index)
+ */
+
+int cmp(char *s1, char *s2, int i, int j)
+{
+	if (strlen(s1) == 0 && strlen(s2) == 0)
+		return (1);
+	if (s2[j] == '*')
+		cmp(s1, s2, i, j + 1);
+	if (s1[i] == s2[j])
+		return (1);
+	return (0);
+}

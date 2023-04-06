95615d8f551c141dadf21d4c39ffde308e06a171
francisthore <thorefrancis@gmail.com>
2023-04-06 14:34:34 -0700

    A little bit grit helps

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,6 @@
 #include <string.h>
 
-int palindrome(char *s, int i);
+int palindrome(char *s, int i, int j);
 
 /**
  * is_palindrome - checks if a string is palindrome
@@ -10,22 +10,26 @@ int palindrome(char *s, int i);
 
 int is_palindrome(char *s)
 {
-	return (palindrome(s, 1));
+	int j = strlen(s) - 1;
+
+	return (palindrome(s, 0, j));
 }
 
 /**
  * palindrome - checks if a string is palindrome
  * @s: string to be checked
+ * @j: recursion looper
  * @i: recursion looper
  * Return: 1 if palindrome, else 0
  */
 
-int palindrome(char *s, int i)
+int palindrome(char *s, int i, int j)
 {
 	if (strlen(s) == 0)
 		return (1);
-	if (s[0] != s[strlen(s) - i])
-		return (0);
-	palindrome(s + 1, i + 1);
-	return (1);
+	if (i >= j)
+		return (1);
+	if (s[i] == s[j])
+		return (palindrome(s, i + 1, j - 1));
+	return (0);
 }

802c5dbe93b453e8fa6bddc8687d8c52deb87eb0
francisthore <thorefrancis@gmail.com>
2023-04-10 06:35:36 -0700

    argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * main - add positive numbers
@@ -21,7 +22,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0)
+			if (!isdigit(*argv[i]))
 			{
 				printf("Error\n");
 				exit(1);

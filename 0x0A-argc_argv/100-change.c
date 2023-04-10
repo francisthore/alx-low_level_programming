36fa529248f6235b3b2228ccbb72152ed074a3a7
francisthore <thorefrancis@gmail.com>
2023-04-10 07:24:05 -0700

    argc, argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - gives change in coins
+ * @argc: numbner of argumts
+ * @argv: array of money
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("Money/25: %d\n", atoi(argv[1]) / 25);
+	printf("Money/25: %d\n", atoi(argv[1]) % 25);
+
+	return (0);
+}

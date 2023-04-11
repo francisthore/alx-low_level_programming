07d03726d7718d22b5781a15a276f46a18378ff5
francisthore <thorefrancis@gmail.com>
2023-04-11 12:46:00 -0700

    Pointers are very important

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * alloc_grid - allocates memory to a grid
+ * @width: grid width
+ * @height: grid height
+ * Return: pointer to grid or NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+		grid[i] = malloc(sizeof(int) * width);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}

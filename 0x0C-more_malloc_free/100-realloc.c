a5de96fb211b3775e82743075afbc675848e41b0
francisthore <thorefrancis@gmail.com>
2023-04-14 08:51:58 -0700

    malloc, free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,6 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
+		free(ptr);
 		ptr = malloc(new_size);
 		if (ptr == NULL)
 			return (NULL);

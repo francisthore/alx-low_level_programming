d297904b0e5b5de247b23f2cc4e36b9ad4af86f5
francisthore <thorefrancis@gmail.com>
2023-04-23 15:24:19 -0700

    preprocessor

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - info about a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H */

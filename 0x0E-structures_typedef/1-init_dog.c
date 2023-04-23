17d7311b0faba7fb197609cba98b25483938583c
francisthore <thorefrancis@gmail.com>
2023-04-23 15:33:33 -0700

    preprocessor

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable dog
+ * @d: pointer to s struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

92929571a27346abbc9a0f3fd021642ab6b7cace
francisthore <thorefrancis@gmail.com>
2023-04-24 08:21:44 -0700

    Function Pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
 {
 	int (*operation)(int, int);
 	int ans;
+	char *o = argv[2];
 
 	if (argc != 4)
 	{
@@ -23,6 +24,12 @@ int main(int argc, char *argv[])
 	}
 
 	operation = get_op_func(argv[2]);
+
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	ans = operation(atoi(argv[1]), atoi(argv[3]));
 

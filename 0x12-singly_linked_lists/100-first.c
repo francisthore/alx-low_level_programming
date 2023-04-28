512918e4f5d6c825a5a441bce54c080f658b744f
Francis Thore <thorefrancis@gmail.com>
2023-04-28 14:38:02 -0700

    betty the better

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * before_main - runs before the main function is called
  */
 
-void __attribute__ ((constructor)) beforemain()
+void __attribute__ ((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
 }

ba95a509e8fddf1bcecc1b29b20121a9ad876894
Francis Thore <thorefrancis@gmail.com>
2023-04-28 13:23:34 -0700

    Data structures lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * free_list - frees a list
+ * @head: first list item
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+		return;
+	tmp = head;
+	while (tmp != 0)
+	{
+		tmp = tmp->next;
+		free(head);
+		head = tmp;
+	}
+	free(tmp);
+}

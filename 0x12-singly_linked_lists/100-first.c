c9f9024c86cefe35873b7114799ff76285e1eadb
Francis Thore <thorefrancis@gmail.com>
2023-04-28 14:40:57 -0700

    betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * before_main - runs before the main function is called
+ * beforemain - runs before the main function is called
  */
 
-void __attribute__ ((constructor)) before_main()
+void __attribute__ ((constructor)) beforemain()
 {
-	printf("You're beat! and yet, you must allow,\n
-	I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }

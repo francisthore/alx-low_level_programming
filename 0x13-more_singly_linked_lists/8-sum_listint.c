28ba5ea6e1490045741f0606c4e9ca7c41d4da22
Francis Thore <thorefrancis@gmail.com>
2023-05-02 12:06:33 -0700

    Summing em all up

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - adds all data of a list
+ * @head: head node of list
+ * Return: sum of all data
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}

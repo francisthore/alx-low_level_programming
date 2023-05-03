497e28ae9e260e44fb728441b96d7856cf9ee36b
Francis Thore <thorefrancis@gmail.com>
2023-05-03 08:30:18 -0700

    reset tmp

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * insert_nodeint_at_index - inserts a node at index
@@ -12,32 +13,31 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_int, *tmp;
-	unsigned int i;
+	unsigned int i = 0, j = 0;
 
 	new_int = malloc(sizeof(listint_t));
 	if (new_int == NULL)
 		return (NULL);
 	new_int->n = n;
 	if (*head == NULL)
-	{
-		new_int->next = NULL;
+	{	new_int->next = NULL;
 		*head = new_int;
 		return (*head);
 	}
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		j++;
+	}
+	if (idx > j)
+		return (NULL);
 	tmp = *head;
 	if (idx == 0)
 	{	new_int->next = tmp;
 		*head = new_int;
 		return (new_int);
 	}
-	i = 0;
-	while (tmp != NULL)
-	{	tmp = tmp->next;
-		i++;
-	}
-	if (idx > i)
-		return (NULL);
-	i = 0;
 	while (i < idx - 1)
 	{
 		if (tmp->next != NULL)

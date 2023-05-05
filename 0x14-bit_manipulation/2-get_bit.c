03fefd8f83aa942d9ec131461a01ebcf72f22b56
Francis Thore <thorefrancis@gmail.com>
2023-05-05 15:39:09 -0700

    Bit manipulations neh

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit at an index
+ * @n: number
+ * @index: index
+ * Return: bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit = (n >> index) & 1;
+
+	return (bit);
+}

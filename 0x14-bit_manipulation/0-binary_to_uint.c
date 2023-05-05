438f67cec50c79a431742735fdb536d44585dc90
Francis Thore <thorefrancis@gmail.com>
2023-05-05 14:52:33 -0700

    Checked for bin

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,12 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
+	for(i = 0; b[i] != '\0'; i++)
+	{
+		if(b[i] != '0' || b[i] != '1')
+			return (0);
+	}
+
 	for (i = 0; b[i] != '\0'; i++)
 		len++;
 	for (i = len - 1; i >= 0; i--)

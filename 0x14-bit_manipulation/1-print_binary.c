cc044efc7fd96429ac1723fbcab3f4d3668eeb9b
Francis Thore <thorefrancis@gmail.com>
2023-05-05 13:56:33 -0700

    I tried using puchar to output the long but eyy

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void print_long_int(unsigned long int n);
+/**
+ * print_binary - prints binary represantation of a number
+ * @n: base 10 number
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int converted = 0, weight = 1, rem;
+
+	while (n != 0)
+	{
+		rem = n & 1;
+		converted = converted + rem * weight;
+		n = n >> 1;
+		weight *= 10;
+	}
+	printf("%lu", converted);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,4 +4,6 @@
 #include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(int ch);
 #endif /* MAIN_H  */

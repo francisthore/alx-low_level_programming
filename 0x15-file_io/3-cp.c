3c574edf29d9181e400c55ff6ef04e2560e15f14
francisthore <thorefrancis@gmail.com>
2023-05-09 22:47:52 +0200

    removed other closes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,12 +27,12 @@ int main(int argc, char **argv)
 	fr = read(file1, buffer, 1024);
 	fw = write(file2, buffer, fr);
 	if (fr == -1)
-	{	close(file1);
+	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (fw == -1)
-	{	close(file2);
+	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}

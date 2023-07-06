9518070102df0c8e81595e14583fa22ca8130ba0
francisthore <thorefrancis@gmail.com>
2023-07-07 00:02:26 +0200

    I see some light

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * set_bit - sets a bit to 1 at a given index
+ * @n: pointer to a number
+ * @index: the position where the bit has to be set
+ * Return: 1 on succes or -1 on failure
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int tmp;
+
+	tmp = *n;
+	tmp = (tmp | (1 << index));
+	*n = tmp;
+
+	return (1);
+}

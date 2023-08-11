00ff771f034ee14023ad91b9428668f5696c9495
francisthore <thorefrancis@gmail.com>
2023-08-11 23:46:04 +0200

    I just had to change order of OPCREAT and O_WR

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,5 @@
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <sys/stat.h>
 /**
  * create_file - creates a file
  * @filename: name of file
@@ -16,16 +14,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename || !text_content)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT, 0666);
-	printf("fd is [%d]\n", fd);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
 	{
-		printf("Failed to create file\n");
-		return (9999);
+		return (-1);
 	}
 	for (i = 0; text_content[i] != '\0'; i++)
 		len++;
 	if (write(fd, text_content, len) == -1)
-		printf("Faile to write \n");
+		return (-1);
 	return (1);
 }

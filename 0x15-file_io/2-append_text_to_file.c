af08cf0d8cf793c505423cf6edf35cf9f602c98e
francisthore <thorefrancis@gmail.com>
2023-08-12 00:04:33 +0200

    Lets append some text shall we

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,24 +1,25 @@
 #include <unistd.h>
 #include <fcntl.h>
+
 /**
- * create_file - creates a file
+ * append_text_to_file - appends text to a file
  * @filename: name of file
- * @text_content: content to be written into the file
+ * @text_content: text to be appended
  * Return: 1 on success or -1 on failure
 */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t i, len = 0;
+	size_t len = 0, i;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
-	{
 		return (-1);
-	}
+	if (!text_content && fd > 0)
+		return (1);
 	for (i = 0; text_content[i] != '\0'; i++)
 		len++;
 	if (write(fd, text_content, len) == -1)

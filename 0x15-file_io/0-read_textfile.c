9134e36570aaa1fda3e4248f398d108278c1dc63
francisthore <thorefrancis@gmail.com>
2023-08-11 22:27:49 +0200

    Added more frees and hand;ed correct le

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * read_textfile - reads a text file and prints in to the std out
@@ -16,21 +17,36 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buff;
 
 	buff = malloc(sizeof(char) * letters);
-	if (!buff)
+	if (buff == NULL)
+	{
+		free(buff);
 		return (0);
+	}
 	/* Handling void filename */
-	if (!filename)
+	if (filename == NULL)
+	{
+		free(buff);
 		return (0);
-	/*Opening the file and handling failure to open */
+	}
+	/* Opening the file and handling failure to open */
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
+	{
+		free(buff);
 		return (0);
+	}
 	letters_read = read(fd, buff, letters);
 	if (letters_read == -1)
+	{
+		free(buff);
 		return (0);
+	}
 	letters_printed = write(1, buff, letters_read);
-	if (letters_printed == -1)
+	if (letters_printed == -1 || letters_printed != letters)
+	{
+		free(buff);
 		return (0);
+	}
 	else
 		return (letters_printed);
 	close(fd);

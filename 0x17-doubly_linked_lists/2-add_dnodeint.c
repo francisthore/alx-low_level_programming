51d8e0f22637f8f5d5f43206662737154d5c492f
Francis Thore <thorefrancis@gmail.com>
2023-09-14 17:26:44 +0200

    Had to check for non-empty list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * add_dnodeint - adds a new node at the begining of list
@@ -10,21 +11,26 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node, *tmp;
+	dlistint_t *new_node;
 
-	if (!*head)
-		return (NULL);
-	tmp = *head;
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
+	if (*head)
+	{
+		new_node->prev = NULL;
+		new_node->n = n;
+		new_node->next = *head;
+		new_node->next->prev = new_node;
+		*head = new_node;
+
+		return (new_node);
+	}
 	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = NULL;
-	new_node->next = tmp;
-	tmp->prev = new_node;
 	*head = new_node;
 
-	return (*head);
+	return (new_node);
 }
 

082bc03f0c063c49540710a7098e36496268a140
Francis Thore <thorefrancis@gmail.com>
2023-09-17 00:41:51 +0200

    A typo cost me 1 hour

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,2 +1,46 @@
 #include <stdlib.h>
 #include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes node at given index
+ * @head: pointer to head node
+ * @index: the index
+ * Return: 1 on success or -1 otherwise
+*/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *tmp, *tmp1;
+
+	if (!head)
+		return (-1);
+	tmp = *head;
+	tmp1 = NULL;
+	if (index == 0)
+	{
+		*head = tmp->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+	for (i = 0; i < index && tmp; i++)
+	{
+		tmp1 = tmp;
+		tmp = tmp->next;
+	}
+
+	if (i < index)
+		return (-1);
+	if (tmp->next)
+	{
+		tmp1->next = tmp->next;
+		tmp->next->prev = tmp->prev;
+		free(tmp);
+		return (1);
+	}
+	tmp1->next = NULL;
+	free(tmp);
+	return (1);
+}

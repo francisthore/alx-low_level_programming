db103ff593c83156d7aa4a0f785d7e3aaa15d9cb
Francis Thore <thorefrancis@gmail.com>
2024-04-10 20:17:09 +0200

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * liner_search - implementation of the linear search algorithm
+ * @array: pointer to the first element of the array
+ * @size: size of the array
+ * @value: value to be search
+ * Return: -1 on failure or index of the element
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t idx;
+	if (!array)
+		return (-1);
+	if (!array[(size - 1)])
+		return (-1);
+	idx = 0;
+	while (idx < size){
+		printf("Value checked array[%ld] = [%d]\n", idx, array[idx]);
+		if (array[idx] == value)
+			return (idx);
+		idx++;
+	}
+	return -1;
+}
